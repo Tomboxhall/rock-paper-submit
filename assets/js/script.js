67c4c7a7f271af6dabbb4d020fed01212d216d6e
Tom Boxhall <bomtoxhall@gmail.com>
2024-02-05 09:31:48 +0000

    add the JS scripts for Player 1 and Computer choices and where they will be shown

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,4 @@
+// Order of the chocies //
 let options = [
     { image: "rock.png", name: "Rock" },
     { image: "paper.png", name: "Paper" },
@@ -6,6 +7,7 @@ let options = [
     { image: "spock.png", name: "Spock" },
 ];
 
+// The rules who who wins //
 let rules = {
     Rock: ["Lizard", "Scissors"],
     Paper: ["Rock", "Spock"],
@@ -13,4 +15,18 @@ let rules = {
     Lizard: ["Spock", "Paper"],
     Spock: ["Scissors", "Rock"],
 }
+
+let imageFolderPath = "assets";
+
+// All of Player 1's choices//
+let player1Choices = document.querySelectorAll("#player1 .available-choices .choices");
+
+//All of the Computer's choices//
+let computerOptions = document.querySelectorAll("#computer .available-choices .choices");
+
+// Where the players chosen choice will show //
+let playerShowArea = document.querySelector("#player1 .selected-choice .choice");
+
+// Where the computers chosen choice will show //
+let computerShowArea = document.querySelector("#computer .selected-choice .choice");
 
