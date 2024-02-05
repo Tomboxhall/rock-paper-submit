cd4cf009b33cd6d799495e595ba18357e5cadea5
Tom Boxhall <bomtoxhall@gmail.com>
2024-02-05 09:14:30 +0000

    add the array sdcript for the game choices

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,8 @@
-console.log("Hello world!");
+let options = [
+    { image: "rock.png", name: "Rock" },
+    { image: "paper.png", name: "Paper" },
+    { image: "scissors.png", name: "Scissors" },
+    { image: "lizard.png", name: "Lizard" },
+    { image: "spock.png", name: "Spock" },
+];
+
