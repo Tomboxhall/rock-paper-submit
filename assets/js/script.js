60c16eb0ec2919e2df4de3e9f615840c26775efe
Tom Boxhall <bomtoxhall@gmail.com>
2024-02-05 16:57:23 +0000

    make alterations to the image function on line 19

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,11 +1,11 @@
 // Order of the chocies //
 let arr = [
-    { image: "rock.png", name: "Rock" },
+    { image: "rock.webp", name: "Rock" },
     { image: "paper.png", name: "Paper" },
     { image: "scissors.png", name: "Scissors" },
     { image: "lizard.png", name: "Lizard" },
     { image: "spock.png", name: "Spock" },
-];
+  ];
 
 // The rules who who wins //
 let rule = {
@@ -14,15 +14,15 @@ let rule = {
     Scissors: ["Paper", "Lizard"],
     Lizard: ["Spock", "Paper"],
     Spock: ["Scissors", "Rock"],
-}
+};
 
-let imageFolderPath = "assets";
+let imageFolderPath = "/assets/images";
 
 // All of Player 1's choices//
-let player1Choices = document.querySelectorAll("#player1 .available-choices .choices");
+let player1Choices = document.querySelectorAll("#player1 .available-choices .choice");
 
 //All of the Computer's choices//
-let computerChoices = document.querySelectorAll("#computer .available-choices .choices");
+let computerChoices = document.querySelectorAll("#computer .available-choices .choice");
 
 // Where the players chosen choice will show //
 let playerShowArea = document.querySelector("#player1 .selected-choice .choice");
@@ -39,75 +39,76 @@ let roundMessage = document.querySelector("#round-message");
 
 // Event listeners for the Player 1 choices //
 player1Choices.forEach((choice) => {
-    choice.addEventListener("click", () => playRound(chocie));
-});
+    choice.addEventListener("click", () => playRound(choice));
+  });
 
 // Play Round function of the game //
 let playRound = (player1Choice) => {
     let player1Index = player1Choice.getAttribute("data-index");
     let computerIndex = Math.floor(Math.random() * arr.length);
-
+  
     showChoiceAndHighlight(player1Index, player1Choices, playerShowArea);
     showChoiceAndHighlight(computerIndex, computerChoices, computerShowArea);
-
+  
     calculateAndDisplayResult(player1Index, computerIndex);
-};
+  };
 
 // Players selected choice //
 let showPlayerChoice = (choice, showArea) => {
     let imgElement = generateImgElement(choice);
     showArea.innerHTML = "";
     showArea.appendChild(imgElement);
-}
+  };
 
-let showChoiceAndHighlight = (index, choice, showArea) => {
+  let showChoiceAndHighlight = (index, choices, showArea) => {
     let selectedChoice = arr[index];
     showPlayerChoice(selectedChoice, showArea);
     highlightSelectedChoice(choices, index);
-};
+  };
 
 // Show Players choice Image //
-let generateImgElement = ({ image, name}) => {
+let generateImgElement = ({ image, name }) => {
     let imgElement = document.createElement("img");
     imgElement.src = `${imageFolderPath}/${image}`;
     imgElement.alt = name;
-    imgElement.title =name;
+    imgElement.title = name;
     return imgElement;
-};
+  };
 
 // Add and remove active class //
 let highlightSelectedChoice = (choices, index) => {
     choices.forEach((choice) => choice.classList.remove("active"));
     choices[index].classList.add("active");
-}
+  };
+  
 
 // Display, and calculate the results //
 let calculateAndDisplayResult = (player1Index, computerIndex) => {
     let player1Choice = arr[player1Index].name;
     let computerChoice = arr[computerIndex].name;
-
+  
     if (player1Choice === computerChoice) {
-        showMessage ("Oh no, it's a draw!");
+      showMessage("Oh no, it's a draw!");
     } else {
-        let player1Strength = rule[player1Choice];
-        if (player1Strength.includes(computerChoice)) {
-            addScore(player1Score);
-            showMessage("Player 1 is the winner!");
-        } else {
-            addScore(computerScore);
-            showMessage("Computer is the winner!");
-        }
+      let player1Strength = rule[player1Choice];
+      if (player1Strength.includes(computerChoice)) {
+        addScore(player1Score);
+        showMessage("Player 1 wins!");
+      } else {
+        addScore(computerScore);
+        showMessage("Bot wins!");
+      }
     }
-};
+  };
 
 // Add Score //
 let addScore = (scoreElement) => {
     scoreElement.textContent = String(Number(scoreElement.textContent) + 1);
-};
+  };
 
-let showMessage = (msg) => {
+  let showMessage = (msg) => {
     roundMessage.innerHTML = msg;
-};
+  };
 
 // Reset Button //
 document.querySelector(".reset-btn").addEventListener("click", resetGame);
@@ -115,27 +116,27 @@ document.querySelector(".reset-btn").addEventListener("click", resetGame);
 function resetGame() {
     resetArea(playerShowArea);
     resetArea(computerShowArea);
-    resetArea(player1Score);
-    resetArea(computerScore);
+    resetScore(player1Score);
+    resetScore(computerScore);
     resetMessage();
     resetChoices(player1Choices);
     resetChoices(computerChoices);
-};
+  };
 
-function resetArea(area) {
+  function resetArea(area) {
     area.innerHTML = "";
-};
+  };
 
-function resetScore(scoreElement) {
+  function resetScore(scoreElement) {
     scoreElement.textContent = "0";
-};
+  };
 
-function resetMessage() {
-    roundMessage.innerHTML = "Make your Choice";
-};
+  function resetMessage() {
+    roundMessage.innerHTML = "Choose your option";
+  };
 
-function resetChoices(choices) {
+  function resetChoices(choices) {
     choices.forEach((choice) => choice.classList.remove("active"));
-};
+  };
 
 
